63c09b45751c19efbae2981a4fcfaf5c832c05dc
zia-viabletree <zia.urrehman@viabletree.com>
2023-10-26 17:04:25 +0500

    detailed screen meter work

--- src/components/Meter/index.jsx
+++ src/components/Meter/index.jsx
@@ -14,37 +14,41 @@ function GraphRender({ stock }) {
   console.log('🚀 ~ file: index.jsx:13 ~ GraphRender ~ stock:', stock);
 
   const meterValue =
-    stock?.overallTrend?.toLowerCase() === ' strong buy'
+    stock?.overallTrend.toLowerCase() === ' strong buy'
+      ? 550
+      : stock?.overallTrend.toLowerCase() === ' buy'
       ? 450
-      : stock?.overallTrend?.toLowerCase() === ' buy'
-      ? 350
-      : stock?.overallTrend?.toLowerCase() === ' sell'
+      : stock?.overallTrend.toLowerCase() === ' sell'
       ? 145
-      : stock?.overallTrend?.toLowerCase() === ' strong sell'
+      : stock?.overallTrend.toLowerCase() === ' strong sell'
       ? 50
-      : 250;
+      : stock?.overallTrend.toLowerCase() === ' neutral sell'
+      ? 250
+      : 350;
 
   const backgroundColor =
-    stock?.overallTrend?.toLowerCase() === ' strong buy'
-      ? 'rgba(25, 62, 29, 0.7)'
-      : stock?.overallTrend?.toLowerCase() === ' buy'
-      ? 'rgba(200, 170, 48, 0.3)'
-      : stock?.overallTrend?.toLowerCase() === ' sell'
-      ? 'rgba(247, 127, 60, 0.3)'
-      : stock?.overallTrend?.toLowerCase() === ' strong sell'
-      ? 'rgba(235, 33, 39, 0.3)'
-      : 'rgba(247, 236, 53, 0.3)';
+    stock?.overallTrend.toLowerCase() === ' strong buy'
+      ? '#8FC64033'
+      : stock?.overallTrend.toLowerCase() === ' buy'
+      ? '#EECB3933'
+      : stock?.overallTrend.toLowerCase() === ' sell'
+      ? '#F77F3C33'
+      : stock?.overallTrend.toLowerCase() === ' strong sell'
+      ? '#EB212733'
+      : stock?.overallTrend.toLowerCase() === ' neutral sell'
+      ? '#FFCF6533'
+      : '#89740933';
 
-  const textColor =
-    stock?.overallTrend?.toLowerCase() === ' strong buy'
-      ? '#3DB54A'
-      : stock?.overallTrend?.toLowerCase() === ' buy'
-      ? '#DEBC31'
-      : stock?.overallTrend?.toLowerCase() === ' sell'
-      ? '#E67230'
-      : stock?.overallTrend?.toLowerCase() === ' strong sell'
-      ? '#EB2127'
-      : '#F7EC35';
+  // const textColor =
+  //   stock?.overallTrend?.toLowerCase() === ' strong buy'
+  //     ? '#3DB54A'
+  //     : stock?.overallTrend?.toLowerCase() === ' buy'
+  //     ? '#DEBC31'
+  //     : stock?.overallTrend?.toLowerCase() === ' sell'
+  //     ? '#E67230'
+  //     : stock?.overallTrend?.toLowerCase() === ' strong sell'
+  //     ? '#EB2127'
+  //     : '#F7EC35';
 
   const options = {
     tooltip: {
@@ -56,7 +60,7 @@ function GraphRender({ stock }) {
     title: {
       text: stock?.overallTrend?.toUpperCase(),
       style: {
-        color: textColor // Specify your desired color here
+        color: stock?.color // Specify your desired color here
       }
     },
 
@@ -94,7 +98,7 @@ function GraphRender({ stock }) {
       lineWidth: 0,
 
       min: 0,
-      max: 500,
+      max: 600,
       labels: {
         enabled: false // Disable the axis labels for the Y-axis
       },
@@ -104,72 +108,37 @@ function GraphRender({ stock }) {
         {
           from: 0,
           to: 100,
-          color: {
-            linearGradient: { x1: 0.1445, x2: 0.8459, y1: 0.1445, y2: 0.8459 },
-            stops: [
-              [0.1445, '#D34E4B'],
-              [0.8459, '#89100E']
-            ]
-          },
+          color: '#8C1912',
           thickness: 65
-          // label: {
-          //   text: 'Strong Buy',
-          //   align: 'center',
-          //   style: {
-          //     color: '#ffffff',
-          //     fontSize: '8px'
-          //   },
-          //   verticalAlign: 'bottom',
-          //   y: 10,
-          //   x: 20
-          // }
         },
         {
           from: 105,
           to: 200,
-          color: {
-            linearGradient: { x1: 0.1504, x2: 0.8328, y1: 0.1504, y2: 0.8328 },
-            stops: [
-              [0.1504, '#F77F3C'],
-              [0.8328, '#A33D03']
-            ]
-          },
+          color: '#8C4213',
           thickness: 65
         },
         {
           from: 205,
           to: 300,
-          color: {
-            linearGradient: { x1: 0, x2: 1, y1: 0, y2: 0 },
-            stops: [
-              [-0.1818, '#FFCF65'],
-              [1, '#9E6D00']
-            ]
-          },
+          color: '#F7AF0F',
           thickness: 65
         },
         {
           from: 305,
           to: 400,
-          color: {
-            linearGradient: { x1: 0.2084, x2: 0.858, y1: 0.2084, y2: 0.858 },
-            stops: [
-              [0.2084, '#EECB39'],
-              [0.858, '#6F5900']
-            ]
-          },
+          color: '#897409',
           thickness: 65
         },
         {
           from: 405,
           to: 500,
-          color: {
-            linearGradient: { x1: 0.1763, x2: 0.8741, y1: 0.1763, y2: 0.8741 },
-            stops: [
-              [0.1763, '#71DF3E'],
-              [0.8741, '#2A7707']
-            ]
-          },
+          color: '#5E7410',
+          thickness: 65
+        },
+        {
+          from: 505,
+          to: 600,
+          color: '#2E6D14',
           thickness: 65
         }
       ]
